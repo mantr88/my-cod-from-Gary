fbdceaea682fa12d4473d5529cfe447caf235991
AntonPetrenko <paa2017sh@gmail.com>
2023-03-18 18:51:11 +0200

    10-19 done

--- src/index.js
+++ src/index.js
@@ -1,73 +1,208 @@
-import { getData } from './api';
-import './css/common.css'
+import './components/Todos';
 
-const HITS_PER_PAGE = 5;
-let page = 0;
-let query = '';
-let pages = 0;
-let items = [];
+// import { Timer } from './widget/timer';
+// import { Clock } from './widget/clock';
 
+// const timer1 = new Timer({ selector: '.timer-1' });
+// const timer2 = new Timer({ selector: '.timer-2', start: 20 });
+// const timer3 = new Timer({ selector: '.timer-3', start: 30 });
 
-const refs = {
-    form: document.querySelector('.form'),
-    list: document.querySelector('.list'),
-    buttons: document.querySelector('.buttons'),
-};
+// const clock = new Clock({ selector: '.clock' });
 
-const render = () => {
-    const list = items.map(({ title, url }) => `<li>
-        <a href="${url}" target="_blank">${title}</a></li>`).join('');
+// ------- new Promise -------
+// const fn1 = () =>
+//   new Promise(resolve => {
+//     setTimeout(() => {
+//       console.log('fn 1');
+//       resolve(1);
+//     }, 700);
+//   });
 
-    refs.list.innerHTML = '';
-    refs.list.insertAdjacentHTML('beforeend', list);
-};
+// const fn2 = x =>
+//   new Promise(resolve => {
+//     setTimeout(() => {
+//       console.log('fn 2');
+//       resolve(x + 1);
+//     }, 450);
+//   });
 
-const renderButtons = () => {
-    refs.buttons.innerHTML = '';
-    const buttons = Array(pages)
-    .fill()
-    .map(
-      (_, idx) =>
-        `<button ${
-            idx === page ? 'class="page active"' : 'class="page"'} 
-          data-page=${idx}>${idx + 1}</button>`,
-    )
-        .join('');
-    
-    refs.buttons.insertAdjacentHTML('beforeend', buttons);
-    
-};
+// const fn3 = x =>
+//   new Promise(resolve => {
+//     setTimeout(() => {
+//       console.log('fn 3');
+//       resolve(x + 1);
+//     }, 500);
+//   });
 
-const fetchNews = () => {
-    getData(query, HITS_PER_PAGE, page)
-        .then((data) => {
-            items = data.hits;
-            pages = data.nbPages;
+// const fn4 = x =>
+//   new Promise(resolve => {
+//     setTimeout(() => {
+//       console.log('fn 4');
+//       resolve(x + 1);
+//     }, 500);
+//   });
 
-            render();
-            renderButtons();
-        })
-        .catch((error) => { console.log('error:', error) });
-};
+// fn1()
+//   .then(res => {
+//     return fn2(res);
+//   })
+//   .then(res => fn3(res))
+//   .then(fn4)
+//   .then(res => {
+//     console.log(res);
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    const { value } = e.currentTarget.elements.query;
-    
-    // перевірка для повернення на першу сорінку при наступному запиті
-    if (query === value || !value) {
-        return
-    };
-    query = value;
-    page = 0;
-    fetchNews();
-};
+//     return res;
+//   })
+//   .then(res => {
+//     console.log(res);
+//   });
 
-const handlePageClick = (e) => { 
-    page = Number(e.target.dataset.page);
-    
-    fetchNews();
-};
+// ----- Promise reject -----
+// const fn1 = () =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log('fn 1');
 
-refs.form.addEventListener('submit', handleSubmit);
-refs.buttons.addEventListener('click', handlePageClick);
+//       reject('error happened');
+//     }, 700);
+//   });
+
+// console.log('spinner on');
+// fn1()
+//   .catch(error => {
+//     console.log('error:', error);
+//   })
+//   .finally(() => {
+//     console.log('spinner off');
+//   });
+
+// console.log('spinner on');
+// fetch('https://hn.algolia.com/api/v1/search?query=cats')
+//   .then(res => {
+//     return res.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   });
+// console.log('spinner off');
+
+// ----- Promise.all / allSettled -------
+// const fn1 = () =>
+//   new Promise(resolve => {
+//     setTimeout(() => {
+//       console.log('fn 1');
+//       resolve(1);
+//     }, 700);
+//   });
+
+// const fn2 = () =>
+//   new Promise(resolve => {
+//     setTimeout(() => {
+//       console.log('fn 2');
+//       resolve(2);
+//     }, 450);
+//   });
+
+// const fn3 = () =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log('fn 3');
+//       reject('error happened');
+//     }, 500);
+//   });
+
+// const fn4 = () =>
+//   new Promise(resolve => {
+//     setTimeout(() => {
+//       console.log('fn 4');
+//       resolve(4);
+//     }, 460);
+//   });
+
+// console.log('start');
+
+// // fn1()
+// //   .then(fn2)
+// //   .then(fn3)
+// //   .then(fn4)
+// //   .then(() => {
+// //     console.log('finish');
+// //   });
+
+// Promise.all([fn1(), fn2(), fn3(), fn4()])
+//   .then(res => {
+//     console.log('finish:', res);
+//   })
+//   .catch(error => {
+//     console.log('error:', error);
+//   });
+
+// Promise.allSettled([fn1(), fn2(), fn3(), fn4()])
+//   .then(res => {
+//     console.log('finish:', res);
+//   })
+//   .catch(error => {
+//     console.log('error:', error);
+//   });
+
+// ------ Promise.resolve / Promise.reject ------
+// [ { name: 'Bobby', age: 15 }, { name: 'Peter', age: 20 } ]
+
+// const getData = () => {
+//   const mockData = [
+//     { name: 'Bobby', age: 15 },
+//     { name: 'Peter', age: 20 },
+//   ];
+
+//   return Promise.resolve(mockData);
+//   // return Promise.reject('error happened');
+// };
+
+// const doSomething = () => {
+//   getData()
+//     .then(data => {
+//       console.log(data);
+//     })
+//     .catch(error => {
+//       console.log('error:', error);
+//     });
+// };
+
+// doSomething();
+
+// ----- Promise.resolve ------
+// const sayHello = (isOk = true) => {
+//   console.log('Hello');
+
+//   return isOk ? Promise.resolve() : Promise.reject('error happaned');
+// };
+
+// const sayBye = () => {
+//   console.log('Bye');
+// };
+
+// sayHello().then(() => {
+//   sayBye();
+// });
+
+// sayHello(false)
+//   .then(() => {
+//     sayBye();
+//   })
+//   .catch(error => {
+//     console.log('error:', error);
+//   });
+
+// ---- hw 6 ----
+// const categories = document.querySelector('#categories');
+// const items = categories.querySelectorAll('.item');
+
+// console.log(`Number of categories: ${items.length}`);
+
+// items.forEach(item => {
+//   const header = item.querySelector('h2').textContent;
+//   const elements = item.querySelectorAll('li');
+
+//   console.log(`Category: ${header}`);
+//   console.log(`Elements: ${elements.length}`);
+// });
